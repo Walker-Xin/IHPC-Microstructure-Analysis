f2b271be33717f60d6b7aedcbe1985110129e29f
Xin Wenkang <walkerxin@outlook.com>
2020-11-11 21:16:02 +0800

    Updated MIPAR images

--- image_saving.py
+++ image_saving.py
@@ -12,19 +12,19 @@ import image_processing
 os.chdir('Data')
 
 # Setting parameters
-rectangular_masks = [(-52, 60), (75, 45), (89.9, 30), (60, 25)]  # FFT masks
+rectangular_masks = [(-30, 50), (65, 45), (89.9, 40)]  # FFT masks
 
 (thersh, kernel, thresh_pre, dia_iter) = (
-    0.24, (3, 3), 25, 3)  # Watershed segmentation
+    0.21, (5, 5), 65, 2)  # Watershed segmentation
 
-merge_thresh = 6500
+merge_thresh = 3000
 
 # Measure run time
 start = time.time()
 
 # Load Image
-image_name = 'IHPC.png'
-name = 'IHPC'
+image_name = 'MIPAR.png'
+name = 'MIPAR'
 image = cv2.imread(
     image_name)
 image_ori = cv2.imread(
@@ -71,21 +71,22 @@ segmented = watershed.watershed(
 
 # Reducing oversegmentation
 unmerged = segmented['modified markers']
-merged = oversegmentation.auto_merge(
+'''merged = oversegmentation.auto_merge(
     segmented['modified markers'], merge_thresh)
 merged = oversegmentation.auto_merge(merged, merge_thresh)
-removed = oversegmentation.remove_boundary(merged)
+removed = oversegmentation.remove_boundary(merged)'''
 
 # Save segmentation results
 image_processing.display_image_2D(
     image_ori,
+    fft_comparison['after FFT inverse'],
     segmented['segmented image'],
     unmerged,
-    removed,
     rows=2, cols=2,
-    filename='segmentation_{}.png'.format(name))
+    cmap=[None, 'gray', None, 'gist_ncar'],
+    filename='segmentation_{}_FFT.png'.format(name))
 
 end = time.time()
 
 # Print run time
-print('Visualisation took %g seconds to execute.' % round(end-start, 1))
+print('Saving took %g seconds to execute.' % round(end-start, 1))
